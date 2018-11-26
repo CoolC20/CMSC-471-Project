715456bd7f85ed2f9a4e2b3dd9d669f4b84016a8
mferry1 <44303888+mferry1@users.noreply.github.com>
2018-11-26 11:53:29 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,11 @@
-import java.io.*;
-
 public class Main{
 
     public static void main(String[] args) {
-
         boolean gameOver = false;
         //create and fill the data array
         dataArray gameStateArray = new dataArray();
         gameState currentGame = new gameState();
+
         currentGame.gameBoard[6][1] = 'x';
         currentGame.gameBoard[6][2] = 'o';
         currentGame.gameBoard[6][3] = 'x';
@@ -17,16 +15,7 @@ public class Main{
         //gameStateArray.dumpArray();
 
         //AI LOOP
-        System.out.println("Creating Tree...");
-        Tree decisionTree = new Tree(currentGame);
-        System.out.println("Filling Tree...");
-        decisionTree.fillTree(decisionTree.getHead());
-        System.out.println("Computing Game Endings of Tree...");
-        decisionTree.assignEndingsOfTree(decisionTree.getHead(), gameStateArray);
-        System.out.println("Calculating Weights of Tree...");
-        decisionTree.weighTree(decisionTree.getHead());
-        decisionTree.printOptimalGamePath(decisionTree.getHead());
+        System.out.println(Tree.AILoop(currentGame, gameStateArray));
         System.out.println("Program end");
-
     }
 }
