74d2c9cca709e2ed19528ebb87ef813b8cd1ed28
cdcampbell24 <34345885+cdcampbell24@users.noreply.github.com>
2018-11-26 18:31:42 -0500

    Update Board.java

--- Board.java
+++ Board.java
@@ -17,8 +17,12 @@ public class Board extends ConnectFour implements ActionListener{
     private JPanel panel1;
     private JPanel panel2;
 
+    private JFrame frame2;
+    private JPanel panel3;
+    private JLabel draw;
     int turn = 0;
 
+    //Creating the initial board UI
     Board(){
           
         frame = new JFrame("Connect Four");
@@ -32,7 +36,8 @@ public class Board extends ConnectFour implements ActionListener{
         for (int i = 0; i < selection.length; i ++){
             int num = i;
             String str = Integer.toString(num);
-            selection[i] = new JButton(str);
+            //selection[i] = new JButton(str);
+            selection[i] = new JButton();
             selection[i].setPreferredSize(new Dimension(85, 20));
             selection[i].setBackground(Color.lightGray);
             panel1.add(selection[i]);
@@ -47,35 +52,45 @@ public class Board extends ConnectFour implements ActionListener{
                 String str1 = Integer.toString(num1);
                 String str2 = Integer.toString(num2);
                 board2[i][j] = "_";
-                board1[i][j] = new JButton(num1 + " " + num2);
+                //board1[i][j] = new JButton(num1 + " " + num2);
+                board1[i][j] = new JButton();
                 board1[i][j].setPreferredSize(new Dimension(85,85));
                 board1[i][j].setBackground(Color.GREEN);
                 board1[i][j].setOpaque(true);
                 panel2.add(board1[i][j]);
             }            
         }
-
+        frame.setBackground(Color.gray);
+        panel1.setBackground(Color.gray);
+        panel2.setBackground(Color.gray);
         frame.add(panel1, BorderLayout.NORTH);
         frame.add(panel2, BorderLayout.CENTER);
         frame.setVisible(true);
 
     }
-    
-        public void actionPerformed(ActionEvent e){
-            int placePiece = y-1;
 
+        public void actionPerformed(ActionEvent e){
+            
+            int placePiece = y-1;
+            //Check which button was selected
             for (int i = 0 ; i < x; i++){
                 if (e.getSource() == selection[i]){
                     
+                // ********* PLACE PIECE FROM AI *********
+
+                    //Check for the next available spot in column
                     while (placePiece >= 0 && board1[placePiece][i].getBackground() != Color.GREEN){
                             placePiece --;
-                        }
-                        
+                    }
+                    
+                    //When there are no spots in column invalid move message is displayed
                     if (placePiece < 0){
                         System.out.println("Invalid move!");
                         }
                         
+                    //Place the piece in column
                     else if (board1[placePiece][i].getBackground() == Color.GREEN ){
+                        
                         if (turn % 2 == 0){
                             board1[placePiece][i].setBackground(Color.red);
                             board2[placePiece][i] = "x";
@@ -85,13 +100,118 @@ public class Board extends ConnectFour implements ActionListener{
                             board2[placePiece][i] = "o";
                         }
                         turn ++;
+                        
+                        //Check for winner
+                        String player = board2[placePiece][i];
+                        if (checkWin() == true){
+                            for (int k = 0; k < selection.length; k ++){
+                                selection[k].setEnabled(false);
+                            }
+                            winGame(player);
+                            System.out.println("WINNER");
+                        }
+
+                        //Check for draw
+                        if (turn >= 42){
+                            for (int k = 0; k < selection.length; k ++){
+                                selection[k].setEnabled(false);
+                            }
+                            System.out.println("DRAW");
+                            drawGame();
+                        }
+                    
                     }
-                }          
-                
+                }             
             }
+                // ********* PASS IN GAME STATE HERE *********
                 getBoard(board2);
         }
 
+        //Check for a win
+        public Boolean checkWin(){
+            for (int i = 0; i < y; i++){
+                for (int j = 0; j < x; j++){
+                    //Check for horizontal win
+                    if (j < 4){
+                        if (board2[i][j] != "_"){
+                            if (board2[i][j] == board2[i][j+1] && board2[i][j] == board2[i][j+2] && board2[i][j] == board2[i][j+3]){
+                                System.out.println("Horizontal Winner");
+                                return true;
+                            }
+                        }
+                    }
+                    //Check for vertical win
+                    if (i < 3){
+                        if(board2[i][j] != "_"){
+                            if (board2[i][j] == board2[i+1][j] && board2[i][j] == board2[i+2][j] && board2[i][j] == board2[i+3][j]){
+                                System.out.println("Vertical Winner");
+                                return true;
+                            }
+                        }
+                    }
+                    //Check for left diagonal win
+                    if (i < 3 && j < 4){
+                        if (board2[i][j] != "_"){
+                            if (board2[i][j] == board2[i+1][j+1] && board2[i][j] == board2[i+2][j+2] && board2[i][j] == board2[i+3][j+3]){
+                                System.out.println("Left Diagonal Winner");
+                                return true;
+                            }
+                        }
+                    }
+                    //Check for right diagonal win
+                    if (i < 3 && j >= 4){
+                        if (board2[i][j] != "_"){
+                            if (board2[i][j] == board2[i+1][j-1] && board2[i][j] == board2[i+2][j-2] && board2[i][j] == board2[i+3][j-3]){
+                                System.out.println("Right Diagonal Winner");
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        //GUI for if there is a winner
+        public void winGame(String player){
+
+            frame2 = new JFrame();
+            frame2.setSize(300,400);
+            panel3 = new JPanel();
+            
+            if (player == "x"){
+                JLabel win = new JLabel("You win!");
+                panel3.add(win);
+            }
+            else {
+                JLabel lose = new JLabel("You lose!");
+                panel3.add(lose);
+            }
+            frame2.add(panel3);
+            frame2.setVisible(true);
+            frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        }
+
+        //GUI for if there is a draw
+        public void drawGame(){
+
+            frame2 = new JFrame();
+            frame2.setSize(300,400);
+
+            panel3 = new JPanel();
+
+            draw = new JLabel("DRAW!!");
+
+            panel3.add(draw);
+
+            frame2.add(panel3);
+            frame2.setVisible(true);
+            frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        }
+
+        //Get board state
         public String[][] getBoard(String board[][]){
             for (int i = 0; i < y; i ++){
                 for (int j = 0; j < x; j++){
@@ -102,4 +222,7 @@ public class Board extends ConnectFour implements ActionListener{
             return board;
         }
     
+    // ********* Pass in integer for computers turn *********
+    //int computeAI(gameState);
+
     }
