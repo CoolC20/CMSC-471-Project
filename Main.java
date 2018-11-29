3b4ae737af221e69b77516f6ff66f1da65b8db86
mferry1 <44303888+mferry1@users.noreply.github.com>
2018-11-29 08:34:42 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,21 +1,20 @@
 public class Main{
 
     public static void main(String[] args) {
-        boolean gameOver = false;
-        //create and fill the data array
-        dataArray gameStateArray = new dataArray();
         gameState currentGame = new gameState();
 
-        currentGame.gameBoard[6][1] = 'x';
-        currentGame.gameBoard[6][2] = 'o';
-        currentGame.gameBoard[6][3] = 'x';
-        currentGame.gameBoard[6][4] = 'o';
-        currentGame.gameBoard[6][5] = 'x';
-
-        //gameStateArray.dumpArray();
+        currentGame.insertIntoColumn(0,'x');
+        currentGame.insertIntoColumn(0,'o');
+        currentGame.insertIntoColumn(1,'x');
+        currentGame.insertIntoColumn(1,'o');
+        currentGame.insertIntoColumn(2,'x');
 
         //AI LOOP
-        System.out.println(Tree.AILoop(currentGame, gameStateArray));
-        System.out.println("Program end");
+        System.out.println(Tree.AILoop(currentGame));
+
+        //System.out.println("Program end");
+
+        //Board board = new Board();
     }
 }
+
