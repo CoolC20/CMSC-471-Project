ac09a4224306e53028056583469cd6e0ec0ffe90
pm26690 <32080197+pm26690@users.noreply.github.com>
2018-12-05 18:46:51 -0500

    added user input for data collection and playing regularly

--- FinalConnectFour/Easy AI/Board.java
+++ FinalConnectFour/Easy AI/Board.java
@@ -248,82 +248,320 @@ public class Board implements ActionListener{
     
     public static void main(String[] args) {
         Scanner move = new Scanner(System.in);
-        gameState currentGame = new gameState();
 
-        //AI LOOP
-        //System.out.println(Tree.AILoop(currentGame));
+        int mode = 0;
+        
+        // test mode implies that both players are AI, and that they
+        // will play a designated amount of games to populate the statistics
+        // global arrays
+        System.out.println("Test mode: Two AI play each other on X randomly generated boards.");
+        System.out.println("Regular play: User designates which players are AI, plays the game with them.");
+        System.out.print("Enter 0 for test mode, Enter 1 for regular play: ");
+        mode = move.nextInt();
 
-        // controls the state of the game, 1 while playing,
-        // 0 when the game finishes
-        int gameState = 1;
+        while(mode < 0 || mode > 1) {
+            System.out.print("Enter 0 for test mode, Enter 1 for regular play: ");
+            mode = move.nextInt();
+        }
+        
+        System.out.println();
+        
+        // data collection values
+        int test_size = 100; // default 100
+        long [] avg_times;
+        int [] player_wins;
+        
+        if(mode == 0) {
+            // run in testing mode
+            
+            System.out.println("Running in test mode, how many games should be played? (1-10000)");
+            test_size = move.nextInt();
+            
+            while(test_size > 10000 || test_size < 0) {
+                System.out.println("Values 1 - 10000 please");
+                test_size = move.nextInt();
+            } 
+            
+            
+            avg_times = new long[test_size];
+            player_wins = new int[test_size];
+            
+            int printing = 0; // enable or disable printing by altering this value
+            
+            System.out.println();
+            
+            System.out.println("Would you like to enable printing of the boards as they are played? Reccomend to disable for data collection");
+            System.out.println("Enter 0: printing off, Enter 1: printing on ");
+            printing = move.nextInt();
+            
+            while(printing > 1 || printing < 0) {
+                System.out.println("Enter 0: printing off, Enter 1: printing on ");
+                printing = move.nextInt();
+            }
+            
+            System.out.println();
+            
+            // need for finding averages
+            int turn = 1;
+            
+            for(int i = 0; i < test_size; i++) {
+                gameState currentGame = new gameState();
+                
+                System.out.println("Playing game " + (i+1) + "...");
 
-        // number of turns passed
-        int turn = 1;
+                // controls the state of the game, 1 while playing,
+                // 0 when the game finishes
+                int gameState = 1;
 
-        // number of player going, 0 for now but 1 and 2 later
-        char player = ' ';
+                // number of turns passed
+                turn = 1;
 
-        // valid returned by check win
-        boolean win = false;
+                // number of player going, 0 for now but 1 and 2 later
+                char player = ' ';
 
-        // plays the connect four game here
-        while (gameState == 1) {
-                // show the game board
-                currentGame.printGameState();
+                // valid returned by check win
+                boolean win = false;
 
-                // player 1 goes on odd turns, player 2 goes on even
-                if(turn % 2 != 0) {
+                // plays the connect four game here
+                while (gameState == 1) {
+                    // show the game board
+                    if(printing == 1)currentGame.printGameState();
+
+                    // player 1 goes on odd turns, player 2 goes on even
+                    if(turn % 2 != 0) {
                         player = 'x';
-                } else {
+                    } else {
                         player = 'o';
-                }
+                    }
 
-                // for now, ask what move the player would like to
-                // make with text
-                boolean x = false;
-                int y = -1;
-                while(x == false) {
+                    // for now, ask what move the player would like to
+                    // make with text
+                    boolean x = false;
+                    int y = -1;
+                    while(x == false) {
                         // ask player where they want to go
-                        System.out.println("Player " + player + ", which column (0-6) would you like to play?");
-                        // player x is AI, player o is AI
+                        if(printing == 1) System.out.println("Player " + player + ", which column (0-6) would you like to play?");
+
+                        // testing mode, both players are AI
                         if(turn % 2 != 0) {
+                            long begin = System.currentTimeMillis();
                             y = Tree.AILoop(currentGame); // get AI loop value
-                            System.out.println(y);
+                            if(printing == 1) System.out.println(y);
+                            long end = System.currentTimeMillis();
+                            avg_times[i] += end-begin;
                         } else {
+                            long begin = System.currentTimeMillis();
                             y = Tree.AILoop(currentGame); // get AI loop value
+                            if(printing == 1) System.out.println(y);
+                            long end = System.currentTimeMillis();
+                            avg_times[i] += end-begin;
                         }
-                        
 
                         // validate user input
                         while(y < 0 || y > 6) {
-                                System.out.println("Player " + player + ", enter a valid column (0-6): ");
-                                y = move.nextInt();
+                            if(printing == 1) System.out.println("Player " + player + ", enter a valid column (0-6): ");
+                            System.out.println("AI returned an invalid column, something very bad happened");
+                            y = move.nextInt();
                         }
 
                         // validate that the move is possible
                         x = currentGame.hasRoomOnColumn(y);
-                }
+                    }
 
-                // move was validated, play the piece and increment turn
-                currentGame.insertIntoColumn(y, player);
-                turn++;
+                    // move was validated, play the piece and increment turn
+                    currentGame.insertIntoColumn(y, player);
+                    turn++;
 
-                // check win condition
-                win = currentGame.checkWin();
-                if(win) {
-                        currentGame.printGameState();
-                        System.out.println("Game Over: Player " + player + " wins!");
+                    // check win condition
+                    win = currentGame.checkWin();
+                    if(win) {
+                        if(printing == 1)currentGame.printGameState();
+                        if(printing == 1)System.out.println("Game Over: Player " + player + " wins!");
                         gameState = 0;
-                }
+                        if(player == 'x') {
+                            player_wins[i] = 1;
+                        } else {
+                            player_wins[i] = 2;
+                        }
+                    }
 
-                // check draw condition
-                if(turn == 42 && win == false) {
-                        currentGame.printGameState();
-                        System.out.println("Game Over: Draw");
+                    // check draw condition
+                    if(turn == 42 && win == false) {
+                        if(printing == 1)currentGame.printGameState();
+                        if(printing == 1)System.out.println("Game Over: Draw");
                         gameState = 0;
+                        player_wins[i] = 0;
+                    }
                 }
+                
+                avg_times[i] /= turn;
+            }
+        
+        double player1 = 0;
+        double player2 = 0;
+        double draw = 0;
+        
+        double avgTime = 0;
+        
+            
+        // per game results, might get cut off because it's too large
+        System.out.println("GAME RESULTS:");
+        System.out.println("--------------------------------------------------------------");
+        for(int i = 0; i < test_size; i++) {
+            System.out.println("Game " + (i+1) + " outcome: " + player_wins[i]);
+            System.out.println("Game " + (i+1) + " average search time: " + avg_times[i]);
+            System.out.println("--");
+            
+            avgTime += avg_times[i];
+            
+            if(player_wins[i] == 1) {
+                player1++;
+            }
+            
+            if(player_wins[i] == 2) {
+                player2++;
+            }
+            
+            if(player_wins[i] == 0) {
+                draw++;
+            }
         }
+        
+        // calculate percentages and averages
+        player1 /= test_size;
+        player2 /= test_size;
+        draw /= test_size;
+        avgTime /= test_size;
+        
+        // print overall statistics
+        System.out.println();
+        System.out.println("OVERALL STATISTICS:");
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("Player 1 win percentage: " + player1);
+        System.out.println("Player 2 win percentage: " + player2);
+        System.out.println("Draw percentage: " + draw);
+        System.out.println("Average time per turn: " + avgTime);
+            
+        } else {
+            // run in regular mode
+            
+            // ask game mode configuration:
+            System.out.println("Game Mode Configurations:");
+            System.out.println("0: Player 1 - Human, Player 2 - Human");
+            System.out.println("1: Player 1 - Human, Player 2 - AI");
+            System.out.println("2: Player 1 - AI,    Player 2 - Human");
+            System.out.println("3: Player 1 - AI,    Player 2 - AI");
+            System.out.print("Enter Preference: ");
+            int config = move.nextInt();
+            
+            while(config < 0 || config > 3) {
+                System.out.print("Enter Preference: ");
+                config = move.nextInt();
+            }
+            
+            System.out.println();
+            
+            int oneIsAI = 0;
+            int twoIsAI = 0;
+            
+            if(config == 0) {
+                oneIsAI = 0;
+                twoIsAI = 0;
+            }
+            
+            if(config == 1) {
+                oneIsAI = 0;
+                twoIsAI = 1;
+            }
+            
+            if(config == 2) {
+                oneIsAI = 1;
+                twoIsAI = 0;
+            }
+            
+            if(config == 3) {
+                oneIsAI = 1;
+                twoIsAI = 1;
+            }
+            
+            gameState currentGame = new gameState();
+            
+            // controls the state of the game, 1 while playing,
+            // 0 when the game finishes
+            int gameState = 1;
 
+            // number of turns passed
+            int turn = 1;
+
+            // number of player going, 0 for now but 1 and 2 later
+            char player = ' ';
+
+            // valid returned by check win
+            boolean win = false;
+
+            // plays the connect four game here
+            while (gameState == 1) {
+                    // show the game board
+                    currentGame.printGameState();
+
+                    // player 1 goes on odd turns, player 2 goes on even
+                    if(turn % 2 != 0) {
+                            player = 'x';
+                    } else {
+                            player = 'o';
+                    }
+
+                    // for now, ask what move the player would like to
+                    // make with text
+                    boolean x = false;
+                    int y = -1;
+                    while(x == false) {
+                            // ask player where they want to go
+                            System.out.println("Player " + player + ", which column (0-6) would you like to play?");
+
+                            // player x is AI, player o is AI
+                            if(turn % 2 != 0) {
+                                if(oneIsAI == 1) {
+                                    y = Tree.AILoop(currentGame); // get AI loop value
+                                    System.out.println(y);
+                                }
+                            } else {
+                                if(twoIsAI == 1) {
+                                    y = Tree.AILoop(currentGame); // get AI loop value
+                                    System.out.println(y);
+                                }
+                            }
+
+                            // user input (if any user is playing)
+                            while(y < 0 || y > 6) {
+                                    System.out.println("Player " + player + ", enter a valid column (0-6): ");
+                                    y = move.nextInt();
+                            }
+
+                            // validate that the move is possible
+                            x = currentGame.hasRoomOnColumn(y);
+                    }
+
+                    // move was validated, play the piece and increment turn
+                    currentGame.insertIntoColumn(y, player);
+                    turn++;
+
+                    // check win condition
+                    win = currentGame.checkWin();
+                    if(win) {
+                            currentGame.printGameState();
+                            System.out.println("Game Over: Player " + player + " wins!");
+                            gameState = 0;
+                    }
+
+                    // check draw condition
+                    if(turn == 42 && win == false) {
+                            currentGame.printGameState();
+                            System.out.println("Game Over: Draw");
+                            gameState = 0;
+                    }
+            }
+        }
         // close the scanner
         move.close();
     }
